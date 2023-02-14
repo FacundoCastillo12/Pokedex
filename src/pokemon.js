99549a2b7cb4552a3ded209376926fa3e140b6ff
FacundoCastillo12 <facuelfacha15@gmail.com>
2023-02-14 20:46:29 -0300

    actualizar pokedex usando POO
    Se actualiza la pokedex para que use lo que aprendimos en clase 15.

--- src/pokemon.js
+++ src/pokemon.js
@@ -1,19 +1,31 @@
 import { conseguirInformacionPokemonId } from './pokeapi.js';
 
-export function crearElementosTarjeta(respuestaJSON) {
+class Pokemon {
+  constructor(RESPUESTA_JSON) {
+    this.nombre = RESPUESTA_JSON.name;
+    this.id = RESPUESTA_JSON.id;
+    this.experiencia = RESPUESTA_JSON.base_experience;
+    this.imagen = RESPUESTA_JSON.sprites.other.home.front_default;
+    this.altura = RESPUESTA_JSON.height;
+    this.peso = RESPUESTA_JSON.weight;
+    this.tipo = RESPUESTA_JSON.types[0].type.name;
+  }
+}
+
+export function crearElementosTarjeta(pokemon) {
   const ELEMENTOS = `
       <div class="card-group tarjeta-completa">
           <div class="card contenedor-tarjeta">
               <div class="card contenedor-imagen">
-                  <h5 class="card-title nombre-pokemon">${respuestaJSON.name}</h5>
-                  <img src="${respuestaJSON.sprites.other.home.front_default}" class="card-img-top">
+                  <h5 class="card-title nombre-pokemon">${pokemon.nombre}</h5>
+                  <img src="${pokemon.imagen}" class="card-img-top">
               </div>
           <div class="tarjeta-informacion">
-              <p class="card-text id">ID: <strong>${respuestaJSON.id}</strong></p>
-              <p class="card-text exp">EXP BASE: <strong>${respuestaJSON.base_experience}</strong></p>
-              <p class="card-text altura">ALTURA: <strong>${respuestaJSON.height}</strong></p>
-              <p class="card-text peso">PESO: <strong>${respuestaJSON.weight}</strong></p>
-              <p class="card-text tipo">TIPO: <strong>${respuestaJSON.types[0].type.name}</strong></p>
+              <p class="card-text id">ID: <strong>${pokemon.id}</strong></p>
+              <p class="card-text exp">EXP BASE: <strong>${pokemon.experiencia}</strong></p>
+              <p class="card-text altura">ALTURA: <strong>${pokemon.altura}</strong></p>
+              <p class="card-text peso">PESO: <strong>${pokemon.peso}</strong></p>
+              <p class="card-text tipo">TIPO: <strong>${pokemon.tipo}</strong></p>
           </div>
           </div>
       </div> 
@@ -26,6 +38,7 @@ export async function crearTarjetasPokemon(pokemonId, ordenFila, ordenTarjeta) {
   const TARJETA = document.querySelector(
     `.fila-${ordenFila} .tarjeta-${ordenTarjeta}`,
   );
-  const ELEMENTOS = crearElementosTarjeta(RESPUESTA_JSON);
+  const nuevoPokemon = new Pokemon(RESPUESTA_JSON);
+  const ELEMENTOS = crearElementosTarjeta(nuevoPokemon);
   TARJETA.innerHTML = ELEMENTOS;
 }
